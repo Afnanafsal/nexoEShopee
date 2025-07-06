577062d3b20f7e31c4712011e8373ca05900817f
afnanafsal <afnanafsal004@gmail.com>
2025-07-06 11:55:24 +0530

    feat: Add DeliveryAddressBar component to display and manage delivery addresses

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -16,6 +16,7 @@ import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import '../../../utils.dart';
 import '../components/home_header.dart';
+import '../components/delivery_address_bar.dart';
 import 'product_type_box.dart';
 import 'products_section.dart';
 
@@ -85,140 +86,150 @@ class _BodyState extends State<Body> {
     return SafeArea(
       child: RefreshIndicator(
         onRefresh: refreshPage,
-        child: SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(screenPadding)),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(height: getProportionateScreenHeight(15)),
-                HomeHeader(
-                  onSearchSubmitted: (value) async {
-                    final query = value.toString();
-                    if (query.length <= 0) return;
-                    List<String> searchedProductsId;
-                    try {
-                      searchedProductsId = await ProductDatabaseHelper()
-                          .searchInProducts(query.toLowerCase());
-                      // ignore: unnecessary_null_comparison
-                      if (searchedProductsId != null) {
-                        await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SearchResultScreen(
-                              searchQuery: query,
-                              searchResultProductsId: searchedProductsId,
-                              searchIn: "All Products",
-                            ),
-                          ),
-                        );
-                        await refreshPage();
-                      } else {
-                        throw "Couldn't perform search due to some unknown reason";
-                      }
-                    } catch (e) {
-                      final error = e.toString();
-                      Logger().e(error);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text("$error"),
-                        ),
-                      );
-                    }
-                  },
-                  onCartButtonPressed: () async {
-                    bool allowed =
-                        AuthentificationService().currentUserVerified;
-                    if (!allowed) {
-                      final reverify = await showConfirmationDialog(context,
-                          "You haven't verified your email address. This action is only allowed for verified users.",
-                          positiveResponse: "Resend verification email",
-                          negativeResponse: "Go back");
-                      if (reverify) {
-                        final future = AuthentificationService()
-                            .sendVerificationEmailToCurrentUser();
-                        await showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AsyncProgressDialog(
-                              future,
-                              message: Text("Resending verification email"),
-                            );
-                          },
-                        );
-                      }
-                      return;
-                    }
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CartScreen(),
-                      ),
-                    );
-                    await refreshPage();
-                  },
-                ),
-                SizedBox(height: getProportionateScreenHeight(15)),
-                SizedBox(
-                  height: SizeConfig.screenHeight * 0.1,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4),
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      physics: BouncingScrollPhysics(),
-                      children: [
-                        ...List.generate(
-                          productCategories.length,
-                          (index) {
-                            return ProductTypeBox(
-                              icon: productCategories[index][ICON_KEY],
-                              title: productCategories[index][TITLE_KEY],
-                              onPress: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        CategoryProductsScreen(
-                                          key: UniqueKey(),
-                                          productType: productCategories[index][PRODUCT_TYPE_KEY],
-                                        ),
+        child: Column(
+          children: [
+            DeliveryAddressBar(),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(screenPadding),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(height: getProportionateScreenHeight(15)),
+                      HomeHeader(
+                        onSearchSubmitted: (value) async {
+                          final query = value.toString();
+                          if (query.length <= 0) return;
+                          List<String> searchedProductsId;
+                          try {
+                            searchedProductsId = await ProductDatabaseHelper()
+                                .searchInProducts(query.toLowerCase());
+                            // ignore: unnecessary_null_comparison
+                            if (searchedProductsId != null) {
+                              await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SearchResultScreen(
+                                    searchQuery: query,
+                                    searchResultProductsId: searchedProductsId,
+                                    searchIn: "All Products",
                                   ),
-                                );
-                              },
+                                ),
+                              );
+                              await refreshPage();
+                            } else {
+                              throw "Couldn't perform search due to some unknown reason";
+                            }
+                          } catch (e) {
+                            final error = e.toString();
+                            Logger().e(error);
+                            ScaffoldMessenger.of(
+                              context,
+                            ).showSnackBar(SnackBar(content: Text("$error")));
+                          }
+                        },
+                        onCartButtonPressed: () async {
+                          bool allowed =
+                              AuthentificationService().currentUserVerified;
+                          if (!allowed) {
+                            final reverify = await showConfirmationDialog(
+                              context,
+                              "You haven't verified your email address. This action is only allowed for verified users.",
+                              positiveResponse: "Resend verification email",
+                              negativeResponse: "Go back",
                             );
-                          },
+                            if (reverify) {
+                              final future = AuthentificationService()
+                                  .sendVerificationEmailToCurrentUser();
+                              await showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AsyncProgressDialog(
+                                    future,
+                                    message: Text(
+                                      "Resending verification email",
+                                    ),
+                                  );
+                                },
+                              );
+                            }
+                            return;
+                          }
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CartScreen(),
+                            ),
+                          );
+                          await refreshPage();
+                        },
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(15)),
+                      SizedBox(
+                        height: SizeConfig.screenHeight * 0.1,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            physics: BouncingScrollPhysics(),
+                            children: [
+                              ...List.generate(productCategories.length, (
+                                index,
+                              ) {
+                                return ProductTypeBox(
+                                  icon: productCategories[index][ICON_KEY],
+                                  title: productCategories[index][TITLE_KEY],
+                                  onPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            CategoryProductsScreen(
+                                              key: UniqueKey(),
+                                              productType:
+                                                  productCategories[index][PRODUCT_TYPE_KEY],
+                                            ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              }),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(20)),
+                      SizedBox(
+                        height: SizeConfig.screenHeight * 0.5,
+                        child: ProductsSection(
+                          sectionTitle: "Products You Like",
+                          productsStreamController: favouriteProductsStream,
+                          emptyListMessage: "Add Product to Favourites",
+                          onProductCardTapped: onProductCardTapped,
+                        ),
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(20)),
+                      SizedBox(
+                        height: SizeConfig.screenHeight * 0.8,
+                        child: ProductsSection(
+                          sectionTitle: "Explore All Products",
+                          productsStreamController: allProductsStream,
+                          emptyListMessage: "Looks like all Stores are closed",
+                          onProductCardTapped: onProductCardTapped,
+                        ),
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(80)),
+                    ],
                   ),
                 ),
-                SizedBox(height: getProportionateScreenHeight(20)),
-                SizedBox(
-                  height: SizeConfig.screenHeight * 0.5,
-                  child: ProductsSection(
-                    sectionTitle: "Products You Like",
-                    productsStreamController: favouriteProductsStream,
-                    emptyListMessage: "Add Product to Favourites",
-                    onProductCardTapped: onProductCardTapped,
-                  ),
-                ),
-                SizedBox(height: getProportionateScreenHeight(20)),
-                SizedBox(
-                  height: SizeConfig.screenHeight * 0.8,
-                  child: ProductsSection(
-                    sectionTitle: "Explore All Products",
-                    productsStreamController: allProductsStream,
-                    emptyListMessage: "Looks like all Stores are closed",
-                    onProductCardTapped: onProductCardTapped,
-                  ),
-                ),
-                SizedBox(height: getProportionateScreenHeight(80)),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
@@ -234,7 +245,8 @@ class _BodyState extends State<Body> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => ProductDetailsScreen(key: UniqueKey(), productId: productId),
+        builder: (context) =>
+            ProductDetailsScreen(key: UniqueKey(), productId: productId),
       ),
     ).then((_) async {
       await refreshPage();
