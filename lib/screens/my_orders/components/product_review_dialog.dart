47f4f345583f9faa04dc5db86a60f39adb222589
afnanafsal <afnanafsal004@gmail.com>
2025-07-17 15:49:02 +0530

    refactor: Enhance Review model and UI components to support user avatar and name, and implement reply functionality

--- lib/screens/my_orders/components/product_review_dialog.dart
+++ lib/screens/my_orders/components/product_review_dialog.dart
@@ -2,24 +2,19 @@ import 'package:nexoeshopee/components/default_button.dart';
 import 'package:nexoeshopee/models/Review.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../../size_config.dart';
 
 class ProductReviewDialog extends StatelessWidget {
   final Review review;
-  ProductReviewDialog({
-    required Key key,
-    required this.review,
-  }) : super(key: key);
+  ProductReviewDialog({required Key key, required this.review})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
-      title: Center(
-        child: Text(
-          "Review",
-        ),
-      ),
+      title: Center(child: Text("Review")),
       children: [
         Center(
           child: RatingBar.builder(
@@ -29,10 +24,7 @@ class ProductReviewDialog extends StatelessWidget {
             allowHalfRating: false,
             itemCount: 5,
             itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-            itemBuilder: (context, _) => Icon(
-              Icons.star,
-              color: Colors.amber,
-            ),
+            itemBuilder: (context, _) => Icon(Icons.star, color: Colors.amber),
             onRatingUpdate: (rating) {
               review.rating = rating.round();
             },
@@ -58,16 +50,26 @@ class ProductReviewDialog extends StatelessWidget {
         Center(
           child: DefaultButton(
             text: "Submit",
-            press: () {
+            press: () async {
+              // Fetch user avatar and name from Firestore
+              final uid = review.reviewerUid;
+              if (uid != null) {
+                final userDoc = await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(uid)
+                    .get();
+                final data = userDoc.data();
+                if (data != null) {
+                  review.userAvatar = data['display_picture'] ?? null;
+                  review.userName = data['display_name'] ?? 'User';
+                }
+              }
               Navigator.pop(context, review);
             },
           ),
         ),
       ],
-      contentPadding: EdgeInsets.symmetric(
-        horizontal: 12,
-        vertical: 16,
-      ),
+      contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
     );
   }
